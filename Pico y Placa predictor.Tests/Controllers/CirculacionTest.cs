386392f641be71ec4c3e74e1b7bf576bf591533d
Jona <nathanojnabetse@hotmail.com>
2021-03-05 00:24:58 -0500

    Added tests for methods in controller
    Tests verify the correct format of input string for plate, date and hour

--- Pico y Placa predictor.Tests/Controllers/CirculacionTest.cs
+++ Pico y Placa predictor.Tests/Controllers/CirculacionTest.cs
@@ -8,6 +8,9 @@ namespace Pico_y_Placa_predictor.Tests.Controllers
     [TestClass]
     public class CirculacionTest
     {
+        /// <summary>
+        /// Test Methos to verify that the correct vie is returned when CirculacionController/Index is called
+        /// </summary>
         [TestMethod]
         public void TestIndexView()
         {
@@ -15,5 +18,122 @@ namespace Pico_y_Placa_predictor.Tests.Controllers
             var result = controller.Index() as ViewResult;
             Assert.AreEqual("Index", result.ViewName);
         }
+
+        /// <summary>
+        /// Test to verify if a vehicle plate is in the correct format
+        /// </summary>
+        [TestMethod]
+        public void TestPlateCheck_Valid()
+        {
+            var controller = new CirculacionController();
+            var validPlate = controller.PlateCheck("JFK-666");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("KJF-8745");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("PCE-9574");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("LOP-755");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("AWE-4510");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("MDF-000");
+            Assert.IsTrue(validPlate);
+            validPlate = controller.PlateCheck("EPL-420");
+            Assert.IsTrue(validPlate);
+        }
+
+        /// <summary>
+        /// Test to verify if a vehicle plate is in the incorrect format
+        /// </summary>
+        [TestMethod]
+        public void TestPlateCheck_NoValid()
+        {
+            var controller = new CirculacionController();
+            var validPlate = controller.PlateCheck("jfk-666");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("KJF-87");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("*CE-9574");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("EOP755");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("FWE-4510");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("0s-440");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("451-AAA");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("fpl123");
+            Assert.IsFalse(validPlate);
+            validPlate = controller.PlateCheck("AAA-42540");
+            Assert.IsFalse(validPlate);
+        }
+
+        /// <summary>
+        /// Test to verify if a date is in the incorrect format
+        /// </summary>
+        [TestMethod]
+        public void TestDateCheck_Valid()
+        {
+            var controller = new CirculacionController();
+            var validDate = controller.DateCheck("14/01/1997");
+            Assert.IsTrue(validDate);
+            
+        }
+
+        /// <summary>
+        /// Test to verify if a date is in the incorrect format
+        /// </summary>
+        [TestMethod]
+        public void TestDateCheck_NoValid()
+        {
+            var controller = new CirculacionController();
+            var validDate = controller.DateCheck("14-01-1997");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("a");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("14/ 01/1997");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("2001/01/05");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("4/01/1997");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("32/05/1997");
+            Assert.IsFalse(validDate);
+            validDate = controller.DateCheck("05/13/1997");
+            Assert.IsFalse(validDate);
+        }
+
+        /// <summary>
+        /// Test to verify if an hour is in the incorrect format
+        /// </summary>
+        [TestMethod]
+        public void TestHourCheck_Valid()
+        {
+            var controller = new CirculacionController();
+            var validHour = controller.HourCheck("15:59");
+            Assert.IsTrue(validHour);
+
+        }
+
+        /// <summary>
+        /// Test to verify if an hour is in the incorrect format
+        /// </summary>
+        [TestMethod]
+        public void TestHourCheck_NoValid()
+        {
+            var controller = new CirculacionController();
+            var validHour = controller.HourCheck("9:45");
+            Assert.IsFalse(validHour);
+            validHour = controller.HourCheck("15:60");
+            Assert.IsFalse(validHour);
+            validHour = controller.HourCheck("24:00");            
+            Assert.IsFalse(validHour);
+            validHour = controller.HourCheck("a");
+            Assert.IsFalse(validHour);
+            validHour = controller.HourCheck("15 05");
+            Assert.IsFalse(validHour);
+        }
+
     }
 }
--- Pico y Placa predictor/Controllers/CirculacionController.cs
+++ Pico y Placa predictor/Controllers/CirculacionController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +13,47 @@ namespace Pico_y_Placa_predictor.Controllers
         public ActionResult Index()
         {
             return View("Index");
+        }
+
+        /// <summary>
+        /// Checks for valid format string for vehicle plate        
+        /// </summary>
+        /// <param name="plate">String of plate to compare</param>
+        /// <returns>if the plate is valid or not</returns>
+        public bool PlateCheck(string plate)
+        {
+            // Valid plate format = XXX-0000
+            // Admits 3 letters (Except D and F at de beggining) , and at least 3 numbers
+            var validPlate = Regex.Match(plate, "^[^DFa-z0-9!\"#$%&'()*+,-./:;<=>?@\\][^_`{|}~][A-Z]{2}-[0-9]{3,4}$");
+
+            return validPlate.Success;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public bool DateCheck(string date)
+        {
+            // Valid date format = dd/MM/yyyy            
+            var validDate = Regex.Match(date, @"(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$");
+
+            return validDate.Success;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hour"></param>
+        /// <returns></returns>
+        public bool HourCheck(string hour)
+        {
+            // 00:00 
+            // 24 Hour format
+            var validHour = Regex.Match(hour, "^([01][0-9]|2[0-3]):[0-5][0-9]$");
+
+            return validHour.Success;
         }
     }
 }
