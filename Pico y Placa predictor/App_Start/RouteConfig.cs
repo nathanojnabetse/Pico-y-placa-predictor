fbb1dd070a7b1776748277267dad7cd2c9fc454a
Jona <nathanojnabetse@hotmail.com>
2021-03-04 21:10:24 -0500

    Creation of the controller Circulacion and view Index
    Layout modification, footer with contact information added.
    Added styles for footer.

--- Pico y Placa predictor/App_Start/RouteConfig.cs
+++ Pico y Placa predictor/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Pico_y_Placa_predictor
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Circulacion", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Pico y Placa predictor/Controllers/CirculacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pico_y_Placa_predictor.Controllers
+{
+    public class CirculacionController : Controller
+    {
+        // GET: Circulacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
