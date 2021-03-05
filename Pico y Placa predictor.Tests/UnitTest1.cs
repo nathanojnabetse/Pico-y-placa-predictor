3411cd027a750500ffa564367f5228327fe687b9
Jona <nathanojnabetse@hotmail.com>
2021-03-04 20:46:10 -0500

    Packages installed in the project
    Bootstrap 4.6
    FontAwesome 4.7
    FontAwesome.MVC 1.0

---
+++ Pico y Placa predictor.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pico_y_Placa_predictor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Pico y Placa predictor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pico_y_Placa_predictor.MvcApplication" Language="C#" %>
